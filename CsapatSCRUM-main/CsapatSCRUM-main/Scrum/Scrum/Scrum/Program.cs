eefe47b9fd15238e4223f3c8bda093425586e76f
F26855 <uveges.bence@ganziskola.hu>
2026-03-24 10:54:53 +0100

    Add files via upload

---
+++ CsapatSCRUM-main/CsapatSCRUM-main/Scrum/Scrum/Scrum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Feladat feladat = new Feladat();
+            feladat.Menu();
+            
+        }
+
+        
+
+    }
+}
